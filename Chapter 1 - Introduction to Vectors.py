d452c67505dcba9d62efd1c922ff2c163d4ba787
raghavrama21 <raghav.rama46@gmail.com>
2024-08-13 23:36:50 -0400

    chapter 1 intro

---
+++ Chapter 1 - Introduction to Vectors.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Chapter 1: Introduction to Vectors
+# 
+
+# The heart of linear algebra lies in two operations:
+# 1. Sum
+# 2. Product
+# 
+# Let there be two vectors $\mathbf{x}$ and $\mathbf{y}$
+# 
+# We can add them:
+#  $\mathbf{x} + \mathbf{y}$
+# 
+# Let there be two constants $c$ and $d$<br> 
+# We can multiply each vector by each constant to get: $c\mathbf{x} $ and $d\mathbf{y}$<br>
+# A linear combination is the combination of the two operations: $c\mathbf{x} + d\mathbf{y}$
+# 
+# 
+# 
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+
+
